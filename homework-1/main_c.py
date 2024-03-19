19edcdde3418b403d03fce5517e65cd8516dcb58
Pasat Tudor <76231144+TudorPCT@users.noreply.github.com>
2024-03-19 13:16:41 +0200

    Update

--- homework-1/main_c.py
+++ homework-1/main_c.py
@@ -3,11 +3,8 @@ import client
 
 i = 0
 while i < 10:
-
     time.sleep(0.3)
-    # call client.main on different thread
     client.main()
-
     i += 1
 
 client.print_final_statistics()
--- homework-1/main_s.py
+++ homework-1/main_s.py
@@ -1,14 +1,10 @@
 import time
-
 import server
-
 
 i = 0
 while i < 10:
     time.sleep(0.1)
-    # call server.main on different thread
     server.main()
-
     i += 1
 
 server.print_final_statistics()
